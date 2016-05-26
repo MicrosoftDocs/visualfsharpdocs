3822040ed4ae5d1d42b1586cffddb61f8d81d5bb
Vandroiy <georg@michna.com>
2016-05-26 14:02:39 +0200

    - Fixed typos in Map.add/Map.Add example codes; matching output

--- docs/conceptual/snippets/fsmaps/snippet1.fs
+++ docs/conceptual/snippets/fsmaps/snippet1.fs
@@ -1,4 +1,4 @@
     Map.ofList [ (1, "one"); (2, "two") ]
     |> Map.add 0 "zero"
-    |> Map.add 3 "twice"
+    |> Map.add 2 "twice"
     |> Map.iter (fun key value -> printfn "key: %d value: %s" key value)
