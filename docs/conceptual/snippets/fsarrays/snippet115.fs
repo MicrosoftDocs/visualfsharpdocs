cf894363ec4c01fecbc1ab600bd883ae51bf1ab8
liboz <ringil.sword@gmail.com>
2016-07-16 11:01:18 -0400

    fix Seq module references

--- docs/conceptual/snippets/fsarrays/snippet115.fs
+++ docs/conceptual/snippets/fsarrays/snippet115.fs
@@ -1,6 +1,6 @@
 let array1 = [| 1 .. 100 |]
-let printArray anarray = Seq.iter (printf "%A ") anarray; printfn ""
-let arrayResult = Seq.countBy (fun elem ->
+let printArray anarray = Array.iter (printf "%A ") anarray; printfn ""
+let arrayResult = Array.countBy (fun elem ->
     if (elem % 2 = 0) then 0 else 1) array1
 
 printArray arrayResult
