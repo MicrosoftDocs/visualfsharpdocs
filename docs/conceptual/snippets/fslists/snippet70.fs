960028fc7728dee02ea0a44b35574c8d7a12e4d8
liboz <ringil.sword@gmail.com>
2016-07-20 15:10:50 -0400

    array/list.distinct and fixes to seq.distinct

--- docs/conceptual/snippets/fslists/snippet70.fs
+++ docs/conceptual/snippets/fslists/snippet70.fs
@@ -2,9 +2,9 @@ let binary n =
     let rec generateBinary n =
         if (n / 2 = 0) then [n]
         else (n % 2) :: generateBinary (n / 2)
-    generateBinary n |> List.rev |> Seq.ofList
+    generateBinary n |> List.rev
 
 printfn "%A" (binary 1024)
 
-let resultSequence = Seq.distinct (binary 1024)
-printfn "%A" resultSequence
+let resultList = List.distinct (binary 1024)
+printfn "%A" resultList
